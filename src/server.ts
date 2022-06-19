080a3e01bd14eb0ed67a696a44810ebf6754bd7f
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-06-18 21:23:58 -0400

    backend getting data

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
+
+app.use(cors(), express.json({limit: '1mb'}));
+
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('hello');
+});
+
+app.post('/data', (req: Request, res: Response) => {
+  
+  console.log(req.body);
+
+  res.send(req.body)
 });
 
 app.listen(PORT, () => console.log('Running on Port 4000'));
